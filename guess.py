9aad418ca001f9d0776da0006a9ba1b997d190b8
Hernan Provoste <hernanprovostev@outlook.com>
2025-04-12 11:51:22 -0400

    Refactor Logic for the inputs

--- guess.py
+++ guess.py
@@ -3,16 +3,13 @@ import random
 
 guessesTaken = 0
 
-print("Hello! Whats is your name?")
-myName = input()
+myName = input("Hello! Whats is your name?\n")
 
 number = random.randint(1, 20)
 print(f'Well, {myName}, I am thinking of a number between 1 and 20.')
 
 for guessesTaken in range(6):
-    print('Take a guess.')
-    guess = input()
-    guess = int(guess)
+    guess = int(input('Take a guess.\n'))
 
     if guess < number:
         print("Your guess is too low.")
